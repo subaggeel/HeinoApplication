c44775aced3385ce915ddf04352a86e5bd802cca
“Pauliina” <pauliina.parkkinen@metropolia.fi>
2016-02-02 10:08:32 +0200

    Initial commit

---
+++ src/java/com/chat/app/Conversation.java
@@ -0,0 +1,17 @@
+package com.chat.app;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author parkkpau1
+ */
+public class Conversation {
+    
+}
