3baea96ca52674708cc7e0a40b8a4d39ac508442
vominhhoang308 <hoang.vominh96@gmail.com>
2016-02-17 12:01:18 +0200

    Fixed version of Version 3 (small fix in package name)

--- src/java/com/chat/websocket/ChatMessage.java
+++ src/java/com/chat/websocket/ChatMessage.java
@@ -1,4 +1,4 @@
-package com.chatApp.websocket;
+package com.chat.websocket;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
